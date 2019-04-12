93fc23fc1ceb6e6c7bb67270b900834693fcbbb5
Lisa Mae <lisa.mae@anthroware.com>
2019-04-12 16:54:35 -0400

    minor styles and navigation change

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -13,27 +13,16 @@ class Profile extends React.Component {
    };
 render() {
  return (
-   <ScreenContainer style = {{flex: 1, backgroundColor: 'pink'}}>
-
-
-    <View style={{ justifyContent: 'center' }}>
-  {/*}  <Deck
-      data={[
-        { file:'https://i.pinimg.com/originals/56/c9/8a/56c98a363ab953d24d36d247c0b1d9a7.jpg'},
-        { file:'https://imgnooz.com/sites/default/files/wallpaper/nature/60890/beautiful-sky-white-flower-wallpapers-60890-9393731.jpg'},
-        { file:'https://afktravel.com/wp-content/uploads/2014/08/namaqua-flowers-2.jpg'},
-        { file:'https://www.holinnbayside.com/blog/wp-content/uploads/2019/02/renee-fisher-669112-unsplash-260x430.jpg'},
-      ]}
-      swipeOrientation="horiz"
-       /> */}
+   <ScreenContainer style = {{flex: 1, backgroundColor: 'lightblue'}}>
+     <View>
+<Text style ={{fontSize: 18, color: 'grey'}}> Profile Screen</Text>
    </View>
-
-
-
-  <Button title="Go to Profile screen"
-    onPress={() => this.props.navigation.navigate('Profile')}
-   />
-
+   <View style={{top: 100}}>
+   <Button
+     title="Go to Home screen"
+     onPress={() => this.props.navigation.navigate('Home')}
+    />
+</View>
 </ScreenContainer>
 );
 }
