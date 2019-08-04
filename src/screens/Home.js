78316133f293d630258da09a78019972f2cb1d15
Lisa Mae <lisa.mae@anthroware.com>
2019-08-03 22:43:04 -0400

    test: check to see if new props work from react native swiper deck changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Deck from 'react-native-swiper-deck';
-import { Button, View } from 'react-native';
-import {
+import { Button, View, StyleSheet } from 'react-native';
 
-  ScreenContainer,
 
-} from '../components/common';
+const styles = StyleSheet.create({
+  cardStyle: {
 
+  },
+});
+
+// const {} = styles;
 
 class Home extends React.Component {
   static navigationOptions = {
@@ -15,25 +18,30 @@ class Home extends React.Component {
 
   render() {
     return (
-      <ScreenContainer style={{ flex: 1, backgroundColor: 'black' }}>
-
-        <View style={{ right: 15, top: 30 }}>
-          <Deck
-
-            data={[
-              { file: 'https://i.pinimg.com/originals/56/c9/8a/56c98a363ab953d24d36d247c0b1d9a7.jpg' },
-              { file: 'https://imgnooz.com/sites/default/files/wallpaper/nature/60890/beautiful-sky-white-flower-wallpapers-60890-9393731.jpg' },
-              { file: 'https://afktravel.com/wp-content/uploads/2014/08/namaqua-flowers-2.jpg' },
-              { file: 'https://www.holinnbayside.com/blog/wp-content/uploads/2019/02/renee-fisher-669112-unsplash-260x430.jpg' },
-            ]}
-            swipeOrientation="horiz"
+      <View style={{ height: 50, width: 50 }}>
+        <Deck
+          data={[
+            { file: 'https://i.pinimg.com/originals/56/c9/8a/56c98a363ab953d24d36d247c0b1d9a7.jpg' },
+            { file: 'https://afktravel.com/wp-content/uploads/2014/08/namaqua-flowers-2.jpg' },
+          ]}
+          swipeOrientation="horiz"
+          imageStyle={{ height: 180 }}
+          deckStyle={{ width: 50, left: 150, top: 150 }}
+          textStyle={{
+            color: 'purple',
+            backgroundColor: 'yellow',
+            fontSize: 12,
+            left: 50,
+            top: 40,
+          }}
+        />
+        <View style={{ flex: 1, width: 120, left: 200 }}>
+          <Button
+            title="Go to Next"
+            onPress={() => this.props.navigation.navigate('Profile')}
           />
         </View>
-        <Button
-          title="Go to Profile screen"
-          onPress={() => this.props.navigation.navigate('Profile')}
-        />
-      </ScreenContainer>
+      </View>
     );
   }
 }
